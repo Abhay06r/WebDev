7449b48e1c3d983ce847a98b3f5ca9a5938d6c6b
Abhay Ojha <abhay06r@gmail.com>
2024-08-25 16:35:46 +0530

    JavaScript

---
+++ JavaScript/in.js
@@ -0,0 +1,21 @@
+const product = {
+    name: "Parker jotter ball pen",
+    Rating: 4,
+    isDeal: true,
+    price: 270,
+    offer: 5
+};
+
+console.log(product);
+
+const profile = {
+    userName: "@shradhakhapra",
+    isFollow: false,
+    follower: "569k",
+    posts: 195,
+    fullName: "Shradha Khapra",
+    designation: "Entrepreneur",
+    bio: " Apna colloge | Ex-Microsoft, DARDO , To educate someone is the privilege"
+};
+
+console.log(profile);
